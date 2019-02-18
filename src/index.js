80af78053e207544cbf9d10fe0cef03ea729ed53
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-18 16:49:48 +0200

    remove unused code

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import ReactDOM from 'react-dom';
 import './theme/init.css';
 
 // App
-// import { Player } from './components-2/Player';
-// ReactDOM.render(<Player />, document.getElementById('root'));
-
-// App
 import { Search } from './Search';
 
 ReactDOM.render(<Search />, document.getElementById('root'));
