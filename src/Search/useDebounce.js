e186e4d6ed2e479241e9548b4201bd47509436cc
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-19 19:27:43 +0200

    → Checkpoint at: 20:57:02

--- src/Search/useDebounce.js
+++ src/Search/useDebounce.js
@@ -5,6 +5,9 @@ import { useState, useEffect } from 'react';
  * Наш кастомный хук
  */
 export const useDebounce = (value, delay) => {
+    /**
+     * Стейт и сеттер для нашего debounced-значения
+     */
     const [ debouncedValue, setDebouncedValue ] = useState(value);
 
     useEffect(() => {
